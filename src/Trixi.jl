f16b6ffb25b9f5ac4c65af9505b2276739b2a49d
Lucas Mager <59776189+MagLuc@users.noreply.github.com>
2021-02-09 12:08:08 +0100

    Update Trixi.jl

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -78,7 +78,8 @@ export CompressibleEulerEquations1D, CompressibleEulerEquations2D, CompressibleE
        IdealGlmMhdEquations1D, IdealGlmMhdEquations2D, IdealGlmMhdEquations3D,
        HyperbolicDiffusionEquations1D, HyperbolicDiffusionEquations2D, HyperbolicDiffusionEquations3D,
        LinearScalarAdvectionEquation1D, LinearScalarAdvectionEquation2D, LinearScalarAdvectionEquation3D,
-       LatticeBoltzmannEquations2D, LatticeBoltzmannEquations3D
+       LatticeBoltzmannEquations2D, LatticeBoltzmannEquations3D,
+       HyperbolicAdvectionDiffusionEquations1D
 
 export flux_central, flux_lax_friedrichs, flux_hll, flux_hllc, flux_godunov,
        flux_chandrashekar, flux_ranocha, flux_derigs_etal, flux_kennedy_gruber, flux_shima_etal
